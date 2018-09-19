34a6966c139ab9dc1d15f8f5be5bcb31e34928a4
brenj <brenj@mailbox.org>
2018-09-19 17:25:35 -0500

    Add validateBlock method to blockchain class

--- chain.js
+++ chain.js
@@ -2,8 +2,9 @@ const SHA256 = require('crypto-js/sha256');
 const chainDataAPI = require('./chainDataAPI.js');
 
 class Block {
-  constructor(data, height = 0, previousBlockHash = '') {
-    this.body = data;
+  constructor(
+    body, height = 0, previousBlockHash = '', time = '', hash = '') {
+    this.body = body;
     this.height = height;
     this.previousBlockHash = previousBlockHash;
     this.time = time || new Date().getTime().toString().slice(0, -3);
@@ -25,8 +26,10 @@ class Blockchain {
       this.getBlockHeight().then((height) => {
         if (height === 0) {
           const genesisBlock = new Block('GENESIS');
+          genesisBlock.hash = genesisBlock.getBlockHash();
           this.api.addDataToLevelDB(JSON.stringify(genesisBlock)).then(() => {
             const newBlock = new Block(blockData, 1, genesisBlock.hash);
+            newBlock.hash = newBlock.getBlockHash();
             this.api.addDataToLevelDB(JSON.stringify(newBlock))
               .then(() => resolve(newBlock))
               .catch(error => reject(error));
@@ -34,6 +37,7 @@ class Blockchain {
         } else {
           this.getBlock(height).then((block) => {
             const newBlock = new Block(blockData, height + 1, block.hash);
+            newBlock.hash = newBlock.getBlockHash();
             this.api.addDataToLevelDB(JSON.stringify(newBlock))
               .then(() => resolve(newBlock))
               .catch(error => reject(error));
@@ -50,7 +54,30 @@ class Blockchain {
   getBlock(blockHeight) {
     return new Promise((resolve, reject) => {
       this.api.getLevelDBData(blockHeight)
-        .then(block => resolve(JSON.parse(block)))
+        .then(rawBlockData => JSON.parse(rawBlockData))
+        .then((blockData) => {
+          // Why doesn't spread operator work here?
+          const block = new Block(
+            blockData.body, blockData.height,
+            blockData.previousBlockHash, blockData.time, blockData.hash);
+          resolve(block);
+        })
+        .catch(error => reject(error));
+    });
+  }
+
+  validateBlock(blockHeight) {
+    return new Promise((resolve, reject) => {
+      this.getBlock(blockHeight)
+        .then((block) => {
+          const blockHash = block.hash;
+
+          // Reset hash to compare original data with existing data
+          const blockToValidate = block;
+          blockToValidate.hash = '';
+
+          resolve(blockToValidate.getBlockHash() === blockHash);
+        })
         .catch(error => reject(error));
     });
   }
@@ -65,3 +92,4 @@ class Blockchain {
 // });
 // blockchain.getBlockHeight().then(height => console.log(height));
 // blockchain.getBlock(0).then(block => console.log(block));
+// blockchain.validateBlock(0).then(isValid => console.log(isValid));
